567134cb02cdc378524971aba99134af3b9c3302
Michal Korybski <michalkorybski@michalkskismini.home>
2018-11-12 21:16:05 +0000

    change: multiple bugs go once and main character moves - 2

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = 0;
+    this.x = -40;
     this.y = 80;
     this.rnd = Math.floor(Math.random() * Math.floor(7));
 };
@@ -34,7 +34,6 @@ Enemy.prototype.update = function(dt) {
 // Now instantiate your objects.
 // ADD up to 3 bugs and loop+rotate the with different x, y and delay params
 var bug_1 = new Enemy();
-//bug_1.update(10);
 var bug_2 = new Enemy();
     bug_2.update(5);
     bug_2.y = 150;
@@ -43,6 +42,23 @@ var bug_3 = new Enemy();
     bug_3.update(3.5);
     bug_3.y = 220;
     bug_3.x = -1;
+
+// bugs in a loop
+setInterval(function(){
+    bug_1.update(3);
+    bug_1.y = 80;
+    bug_1.x = -50;
+
+    bug_2.update(9);
+    bug_2.y = 150;
+    bug_2.x = -20;
+
+    bug_3.update(6);
+    bug_3.y = 220;
+    bug_3.x = -100;
+
+}, 2000); // 33 milliseconds = ~ 30 frames per sec
+
 
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [bug_1, bug_2, bug_3];
